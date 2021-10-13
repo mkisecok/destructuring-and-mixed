a02bbe3880eb77e4027fb042e9a8956f788f23d3
Mkisecok <mahmutkisecok@gmail.com>
2021-10-13 20:59:45 +0200

    Add task4

--- index.js
+++ index.js
@@ -34,7 +34,33 @@ console.log(hobbies);
 
 console.log(`=====Task3=====`);
 
-
-
 const entries = Object.entries(person);
+
 console.log(entries);
+
+
+console.log(`=====Task4=====`);
+
+const cities = ["Berlin", "Hamburg", "Leipzig", "Duesseldorf"];
+const inhabitants = ["3,645 million", "1,841 million", "587.857 thousand", "619.294 thousand"];
+
+result = cities.reduce((o, k, i) => ({...o, [k]: inhabitants[i]}), {})
+
+console.log(result);
+
+/* Alternative  with assign
+result2 = Object.assign(...cities.map((k, i) => ({[k]: inhabitants[i]})))
+*/
+
+/*Alternative with for each
+let result3 = {};
+cities.forEach((key, i) => result3[key] = inhabitants[i]);
+ */
+
+/*Alternative with Object.fromEntries
+let result4 = {};
+Object.fromEntries(cities.map((_, i) => [cities[i], inhabitants[i]]));
+ */
+
+
+
