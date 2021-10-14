7716c18a7ce193727ba34952be19504fabd8b52b
Mkisecok <mahmutkisecok@gmail.com>
2021-10-14 14:26:07 +0200

    Change task5

--- index.js
+++ index.js
@@ -20,15 +20,13 @@ const person = {
 
 };
 
-const{firstName}=person;
+const{firstName:name,hobbies}=person;
 
-console.log(firstName);
+console.log(name);
 
 const{city}=person.address;
 
 console.log(city);
-
-const{hobbies} = person;
 
 console.log(hobbies);
 
@@ -43,10 +41,14 @@ console.log(`=====Task4=====`);
 
 const cities = ["Berlin", "Hamburg", "Leipzig", "Duesseldorf"];
 const inhabitants = ["3,645 million", "1,841 million", "587.857 thousand", "619.294 thousand"];
+function ourNewObject(par1,par2)
+{
+   let result = par1.reduce((o, k, i) => ({...o, [k]: par2[i]}), {});
+    return result;
+}
 
-result = cities.reduce((o, k, i) => ({...o, [k]: inhabitants[i]}), {})
 
-console.log(result);
+console.log(ourNewObject(cities,inhabitants));
 
 /* Alternative  with assign
 result2 = Object.assign(...cities.map((k, i) => ({[k]: inhabitants[i]})))
@@ -59,17 +61,28 @@ cities.forEach((key, i) => result3[key] = inhabitants[i]);
 
 /*Alternative with Object.fromEntries
 let result4 = {};
-Object.fromEntries(cities.map((_, i) => [cities[i], inhabitants[i]]));
- */
+result4=Object.fromEntries(cities.map((a,i) => [cities[i], inhabitants[i]]));
+ console.log(result4);
+*/
 
-
-console.log(`==========Task4========`);
+console.log(`==========Task5========`);
 
 const myArr = ["Iran", "Brazil", "Syria", "Canada"];
 
+/*
+let[a,b,c,d]=  myArr;
+d='germany'
+console.log(d);
+console.log(myArr);
+*/
+
 function toNewArray(element)
 {
-
- return [element[1],element[0],element[2],element='Germany']  
+ for(let i=0; i< element.length; i++)
+ {
+     let result= [element[i+1],element[i],element[i+2],element[i+3]='Germany'];
+     return result
+ }
+ //return [element[1],element[0],element[2],element='Germany']  
 }
 console.log(toNewArray(myArr));
